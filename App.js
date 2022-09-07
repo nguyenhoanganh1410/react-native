41135bbb0972c5506beac8aa86077903b4ec3a71
hoanganh <hoanganh1410tb@gmail.com>
2022-09-07 21:20:12 +0700

    finished screen 1

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  TouchableHighlight,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
+import Screen01 from "./components/Screen01";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Screen01 />
     </View>
   );
 }
@@ -13,8 +22,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
